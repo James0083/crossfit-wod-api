fc2b1d72340364a4d50a9468d00fd72cec9bd95f
You JaeWon <71857831+James0083@users.noreply.github.com>
2023-09-21 16:32:53 +0900

    data caching
    using apicache middleware

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const apicache = require("apicache");
 // const v1Router = require("./v1/routes");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express();
+const cache = apicache.middleware;
 const PORT = process.env.PORT || 3000;
 
 /*
@@ -13,6 +15,7 @@ app.get("/", (req, res) => {
 */
 
 app.use(bodyParser.json());
+app.use(cache("2 minutes"));
 // app.use("/api/v1", v1Router);
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,16 +1,22 @@
 const express = require("express");
+const apicache = require("apicache");
+// const authenticate = require("../../middlewares/authenticate");
+// const authorize = require("../../middlewares/authorize");
 const workoutController = require("../../controllers/workoutController");
 const recordController = require("../../controllers/recordController");
 
 const router = express.Router();
+const cache = apicache.middleware;
 
-router.get("/", workoutController.getAllWorkouts);
+// router.get("/", workoutController.getAllWorkouts);
+router.get("/", cache("2 minutes"), workoutController.getAllWorkouts);
 
 router.get("/:workoutId", workoutController.getOneWorkout);
 
 router.get("/:workoutId/records", recordController.getRecordForWorkout);
 
 router.post("/", workoutController.createNewWorkout);
+// router.post("/", authenticate, authorize, workoutController.createNewWorkout);
 
 router.patch("/:workoutId", workoutController.updateOneWorkout);
 
