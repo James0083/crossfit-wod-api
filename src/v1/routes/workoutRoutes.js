49a66cb6a71564cbebbd92ba542f64e424d706ab
You JaeWon <71857831+James0083@users.noreply.github.com>
2023-09-22 10:36:16 +0900

    swagger practice

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const apicache = require("apicache");
+// const apicache = require("apicache");
 // const authenticate = require("../../middlewares/authenticate");
 // const authorize = require("../../middlewares/authorize");
 const workoutController = require("../../controllers/workoutController");
 const recordController = require("../../controllers/recordController");
 
 const router = express.Router();
-const cache = apicache.middleware;
+// const cache = apicache.middleware;
 
 /**
  * @openapi
@@ -34,7 +34,10 @@ const cache = apicache.middleware;
  *                              data:
  *                                  type: array
  *                                  items:
- *                                      $ref: "#/components/schemas/Workout"
+ *                                      type: object
+ *                                      oneOf:
+ *                                        - $ref: "#/components/schemas/Workout"
+ *                                        - $ref: "#/components/schemas/Workout-2"
  *          5XX:
  *              description: FAILED
  *              content:
@@ -52,8 +55,68 @@ const cache = apicache.middleware;
  *                                          type: string 
  *                                          example: "Some error message"
  */
-router.get("/", cache("2 minutes"), workoutController.getAllWorkouts);
+router.get("/", workoutController.getAllWorkouts);
+// router.get("/", cache("2 minutes"), workoutController.getAllWorkouts);
 
+/**
+ * @openapi
+ * /api/v1/workouts/{workoutId}:
+ *  get:
+ *      tags:
+ *          - Workouts
+ *      parameters:
+ *        - in: path
+ *          name: workoutId
+ *          schema:
+ *              type: string
+ *          description: Id of a workout
+ *          example: 4a3d9aaa-608c-49a7-a004-66305ad4ab50
+ *      responses:
+ *          200:
+ *              description: OK
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              status:
+ *                                  type: string
+ *                                  example: OK
+ *                              data:
+ *                                  $ref: "#/components/schemas/Workout-2"
+ *          400:
+ *              description: FAILED
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              status:
+ *                                  type: string
+ *                                  example: FAILED
+ *                              data:
+ *                                  type: object
+ *                                  properties:
+ *                                      error:
+ *                                          type: string
+ *                                          example: "Parameter ':workoutId' can not be empty"
+ *          5XX:
+ *              description: FAILED
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              status: 
+ *                                  type: string
+ *                                  example: FAILED
+ *                              data:
+ *                                  type: object
+ *                                  properties:
+ *                                      error:
+ *                                          type: string 
+ *                                          example: "Some error message"
+ */
 router.get("/:workoutId", workoutController.getOneWorkout);
 
 router.get("/:workoutId/records", recordController.getRecordForWorkout);
